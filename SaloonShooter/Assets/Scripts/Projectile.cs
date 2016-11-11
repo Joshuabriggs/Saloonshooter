a726fc272b6272e35b1e5f6b16bf820c4cb11806
pliskin95 <i7622099>
2016-11-11 23:22:16 +0000

    I was slacking init :ok_hand:

--- SaloonShooter/Assets/Scripts/Projectile.cs
+++ SaloonShooter/Assets/Scripts/Projectile.cs
@@ -5,10 +5,25 @@ public class Projectile : MonoBehaviour {
 
     public float ProjectileSpeed = 100f;
     private Rigidbody rbody;
+    public bool Spin = false;
 
 	// Use this for initialization
 	void Start () {
         rbody = this.GetComponent<Rigidbody>();
         rbody.AddForce(transform.forward * ProjectileSpeed);
+    }
+
+    void Update() {
+        if (Spin) {
+            rbody.AddTorque(transform.right*3000f*Time.deltaTime);
+            //transform.Rotate(1000 * Time.deltaTime,0, 0);
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        if(col.gameObject.tag == "Ground"){
+            Spin = false;
+            //Destroy(rbody);
+        }
     }
 }
