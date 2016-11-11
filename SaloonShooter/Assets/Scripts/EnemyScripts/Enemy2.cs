327aa6b4aea339943509c0df21e5a605a46ca427
Joshuabriggs <i7679794@bournemouth.ac.uk>
2016-11-11 21:22:57 +0000

    Enemy2 and avoiding

--- SaloonShooter/Assets/Scripts/EnemyScripts/Enemy2.cs
+++ SaloonShooter/Assets/Scripts/EnemyScripts/Enemy2.cs
@@ -1,32 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Enemy1 : MonoBehaviour {
+public class Enemy2 : MonoBehaviour {
 
     public int m_health = 10;
     private int m_path;
     [SerializeField]
     private float m_deviation = 60f;
     [SerializeField]
-    private float m_speed = 5f;
+    private float m_speed = 8f;
     [SerializeField]
     private float m_attacktimer = 30f;
+    private float m_traveldistance;
+    private float m_distancetraveled;
+    private Vector3 m_startLocation;
+    private Vector3 m_shotSpawn;
     private int m_travelling;
     private Transform m_transform;
+    private Transform m_guntransform;
     private Rigidbody m_body;
+    public GameObject m_bullet;
+
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
 
         m_transform = transform;
         m_path = Random.Range(1, 4);
         m_travelling = 1;
         m_body = GetComponent<Rigidbody>();
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        m_traveldistance = Random.Range(15, 50);
+        m_startLocation = m_transform.position;
+        m_guntransform = GetComponentInChildren<Transform>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_distancetraveled = m_transform.position.z - m_startLocation.z;
+
+        m_shotSpawn = m_guntransform.position;
+
+        if(m_distancetraveled >= m_traveldistance)
+        {
+            m_travelling = 2;
+        }
 
         if (m_travelling == 1)
         {
@@ -52,14 +73,14 @@ public class Enemy1 : MonoBehaviour {
                 Avoid();
                 break;
         }
-        
+
 
         if (m_health <= 0)
         {
             Destroy(gameObject);
         }
-	
-	}
+
+    }
 
     void Moving()
     {
@@ -90,17 +111,21 @@ public class Enemy1 : MonoBehaviour {
     void Attacking()
     {
 
-        if(m_attacktimer <=0)
+        if (m_attacktimer <= 0)
         {
             m_body.AddForce(new Vector3(0, 100f, 0));
             m_attacktimer = 30f;
+            Instantiate(m_bullet, m_shotSpawn, Quaternion.identity);
         }
 
         m_transform.eulerAngles = new Vector3(0, 0, 0);
 
         m_attacktimer -= 1;
 
-      
+        
+
+
+
     }
 
     void Avoid()
@@ -136,7 +161,7 @@ public class Enemy1 : MonoBehaviour {
         {
             m_health -= 5;
         }
-      
+
 
     }
 
---
+++ SaloonShooter/Assets/Scripts/EnemyScripts/EnemyBulletScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBulletScript : MonoBehaviour {
+
+    [SerializeField]
+    private float m_speed = 20f;
+    private Transform m_transform;
+
+	// Use this for initialization
+	void Start () {
+
+        m_transform = transform;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_transform.Translate(new Vector3(0, 0, 1) * m_speed * Time.deltaTime);
+	}
+}
