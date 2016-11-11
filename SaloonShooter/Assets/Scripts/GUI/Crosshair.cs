461557c8edc050ee91f809b7f94c770f59dfbe5d
pliskin95 <i7622099>
2016-11-11 21:21:44 +0000

    Shooting & Crosshair

---
+++ SaloonShooter/Assets/Scripts/GUI/Crosshair.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crosshair : MonoBehaviour {
+
+    public Texture2D crosshairTexture;
+    private Rect position;
+    static bool OriginalOn = true;
+
+    void Start()
+    {
+        //Cursor.visible = false;
+        position = new Rect((Screen.width - crosshairTexture.width) / 2, (Screen.height - crosshairTexture.height) / 2, crosshairTexture.width, crosshairTexture.height);
+    }
+
+    void OnGUI()
+    {
+        if (OriginalOn == true)
+        {
+            GUI.DrawTexture(position, crosshairTexture);
+        }
+    }
+}
---
+++ SaloonShooter/Assets/Scripts/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+    public float ProjectileSpeed = 100f;
+    private Rigidbody rbody;
+
+	// Use this for initialization
+	void Start () {
+        rbody = this.GetComponent<Rigidbody>();
+        rbody.AddForce(transform.forward * ProjectileSpeed);
+    }
+}
