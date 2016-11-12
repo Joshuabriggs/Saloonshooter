9b28d61f388687b70bf3a77e89bd9944554bf83d
pliskin95 <i7622099>
2016-11-12 16:52:37 +0000

    22

--- SaloonShooter/Assets/Scripts/PlayerScripts/PlayerControll.cs
+++ SaloonShooter/Assets/Scripts/PlayerScripts/PlayerControll.cs
@@ -1,26 +1,48 @@
 ﻿using UnityEngine;
+using UnityStandardAssets.ImageEffects;
 using System.Collections;
 
-public class PlayerControll : MonoBehaviour {
 
-    public float MovementSpeed=5.0f; //How fast does the player move
+public class PlayerControll : MonoBehaviour
+{
+
+    public float MovementSpeed = 8.0f; //How fast does the player move
     public float HideHeight = 50.0f; //How low does the player hide
 
     public float PlayerHealth; //Players health (for later use)
 
     private float currentSpeed; //Players current speed which is affected by crouching
-    private bool isCrouched=false; //Crouch flag
+    [HideInInspector]
+    public int m_drunkspin = 0;
+    private float m_drunkmultiply = 0.5f;
+    private int m_drunkcount = 5;
+    private bool isCrouched = false; //Crouch flag
 
+<<<<<<< HEAD
     private bool keyLeft, keyRight, KeyCrouch;
 
 	// Update is called once per frame
 	void Update () {
+=======
+    // Update is called once per frame
+    void Update()
+    {
+>>>>>>> origin/master
         SpeedControl();
         GetInput();
     }
 
-    void FixedUpdate() {
+    void FixedUpdate()
+    {
         Movement();
+        Drunk();
+
+    }
+
+    void Start()
+    {
+        
+
     }
 
     private void GetInput() {    
@@ -52,8 +74,15 @@ public class PlayerControll : MonoBehaviour {
     }
 
     //Player movement
+<<<<<<< HEAD
     private void Movement() {
         if (keyLeft)
+=======
+    private void Movement()
+    {
+        //Move Left
+        if (Input.GetKey(KeyCode.A))
+>>>>>>> origin/master
         {
             transform.Translate(transform.right * currentSpeed * Time.deltaTime);
         }
@@ -66,29 +95,110 @@ public class PlayerControll : MonoBehaviour {
         //Crouch
         if (KeyCrouch && !isCrouched)
         {
+<<<<<<< HEAD
             crouch();
             transform.Translate(transform.up * -HideHeight * Time.deltaTime);
         }
         if(!KeyCrouch && isCrouched){
+=======
+
+            crouch();
+            transform.Translate(transform.up * -HideHeight * Time.deltaTime);
+        }
+        //Stand Up
+        if (Input.GetKeyDown(KeyCode.W) && isCrouched == true)
+        {
+>>>>>>> origin/master
             stopCrouching();
             transform.Translate(transform.up * HideHeight * Time.deltaTime);
         }
     }
 
-    private void crouch() {
+    private void Drunk()
+    {
+       
+        
+        switch (m_drunkspin)
+        {
+            case 0:
+                break;
+
+            case 1:
+                transform.Rotate(new Vector3(1f, 0, -1) * m_drunkmultiply, Space.World);
+
+                transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, -90, transform.rotation.eulerAngles.z);
+                m_drunkcount++;
+                if (m_drunkcount > 10)
+                {
+                    m_drunkmultiply *= -1;
+                    m_drunkcount = 0;
+                }
+
+                break;
+
+            case 2:
+                transform.Rotate(new Vector3(1, 0, -1) * m_drunkmultiply, Space.World);
+
+                transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, -90, transform.rotation.eulerAngles.z);
+
+                m_drunkcount++;
+                if (m_drunkcount > 20)
+                {
+                    m_drunkmultiply *= -1;
+                    m_drunkcount = 0;
+                }
+
+                break;
+
+            case 3:
+                transform.Rotate(new Vector3(1, 0, -1) * m_drunkmultiply, Space.World);
+
+
+                transform.eulerAngles = new Vector3(transform.rotation.eulerAngles.x, -90, transform.rotation.eulerAngles.z);
+
+
+                m_drunkcount++;
+                if (m_drunkcount > 30)
+                {
+                    m_drunkmultiply *= -1;
+                    m_drunkcount = 0;
+                }
+
+                break;
+
+        }
+
+        if (m_drunkspin > 0)
+        {
+            GetComponentInChildren<BlurOptimized>().enabled = true;
+            GetComponentInChildren<BlurOptimized>().blurSize = m_drunkspin - 1;
+
+        }
+        if (m_drunkspin <= 0)
+        {
+            GetComponentInChildren<BlurOptimized>().enabled = false;
+        }
+
+
+    }
+
+    private void crouch()
+    {
         this.GetComponent<BoxCollider>().size -= new Vector3(0, .8f, 0);
         this.GetComponent<BoxCollider>().center -= new Vector3(0, -.2f, 0);
         isCrouched = true;
     }
 
-    private void stopCrouching() {
+    private void stopCrouching()
+    {
         this.GetComponent<BoxCollider>().size += new Vector3(0, .8f, 0);
         this.GetComponent<BoxCollider>().center += new Vector3(0, -.2f, 0);
         isCrouched = false;
     }
 
     //Depending the state the player is in adjust the speed accordingly
-    private void SpeedControl() {
+    private void SpeedControl()
+    {
         if (isCrouched)
         {   //If the player is crouching half the speed they can move at
             currentSpeed = MovementSpeed / 2f;
@@ -99,3 +209,4 @@ public class PlayerControll : MonoBehaviour {
         }
     }
 }
+
