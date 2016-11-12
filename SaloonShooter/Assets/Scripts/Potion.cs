a0925e69bced2c08a4fcf1b45dfc5187f3292779
Joshuabriggs <i7679794@bournemouth.ac.uk>
2016-11-12 03:37:08 +0000

    Changes before sleepy

---
+++ SaloonShooter/Assets/Scripts/Potion.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Potion : MonoBehaviour {
+
+    private GameObject m_player;
+    private int m_relativeposition;
+    private Transform m_transform;
+    private bool m_close;
+
+
+	// Use this for initialization
+	void Start () {
+
+        m_player = GameObject.FindGameObjectWithTag("Player");
+        m_transform = transform;
+        m_close = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(m_player.transform.position.x >= m_transform.position.x -1 && m_player.transform.position.x <= m_transform.position.x + 1)
+        {
+            m_close = true;
+            
+            if(Input.GetKey(KeyCode.H))
+            {
+                GameState.instance.PlayerHit(-25f);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            m_close = false;
+        }
+	
+	}
+
+    void OnGUI()
+    {
+        if (m_close == true)
+        {
+            GUI.Label(new Rect(10, 40, 100, 20), "Press 'H' to drink");
+        }
+    }
+}
