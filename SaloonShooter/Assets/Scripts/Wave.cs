0b4717da36c54082437cac3bc98157da5f522a5b
Callum Pollock <thedoopz@gmail.com>
2016-11-12 22:26:40 +0000

    :8ball:

--- SaloonShooter/Assets/Scripts/Wave.cs
+++ SaloonShooter/Assets/Scripts/Wave.cs
@@ -4,7 +4,9 @@ using System.Collections;
 [System.Serializable]
 public class Wave
 {
-    public int enemyTypes = 0;
+
+    public int[] enemies;
+
     public float spawnInterval = 2f;
     public int maxEnemies = 10;
 }
